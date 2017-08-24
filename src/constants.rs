48d5ec06bb0d3ec734607b835db3836c5070e5f7
italic <italic.rendezvous@gmail.com>
2017-08-23 23:59:41 -0700

    Fix constants/statics.
    Make byte constants `const` and leave strings as `static`.
    ADDR_INFO_DELIM is now two-byte array. Will likely change in the future.

--- src/constants.rs
+++ src/constants.rs
@@ -1,38 +1,38 @@
 use std::collections::HashMap;
 
-pub static APRSIS_SERVERS: Vec<&'static str> = ["rotate.aprs.net", "noam.aprs2.net"].to_vec();
+pub static APRSIS_SERVERS: &'static [&'static str; 2] = &["rotate.aprs.net", "noam.aprs2.net"];
 pub static APRSIS_SW_VERSION: &'static str = "APRS Rust Module";
 
 lazy_static! {
     pub static ref APRSIS_HTTP_HEADERS: HashMap<&'static str, &'static str> = {
-        let mut h = HashMap::new();
+        let mut h: HashMap<&'static str, &'static str> = HashMap::new();
         h.insert("content-type", "application/octet-stream");
         h.insert("accept", "text/plain");
         h
     };
 }
 
-pub static APRSIS_FILTER_PORT: usize = 14580;
-pub static APRSIS_RX_PORT: usize = 8080;
+pub const APRSIS_FILTER_PORT: usize = 14580;
+pub const APRSIS_RX_PORT: usize = 8080;
 pub static APRSIS_URL: &'static str = "http://srvr.aprs-is.net:8080";
 
-pub static RECV_BUFFER: usize = 1024;
+pub const RECV_BUFFER: usize = 1024;
 
 pub static DEFAULT_TOCALL: &'static str = "APYT70";
 
 /// AX.25 Flag - The flag field at each end of the frame is the
 ///              bit sequence 0x7E that separates each frame.
-pub static AX25_FLAG: u8 = 0x7E;
+pub const AX25_FLAG: u8 = 0x7E;
 /// AX.25 Control Field - This field is set to 0x03 (UI-frame).
-pub static AX25_CONTROL_FIELD: u8 = 0x03;
+pub const AX25_CONTROL_FIELD: u8 = 0x03;
 /// AX.25 Protocol ID - This field is set to 0xF0 (no layer 3 protocol).
-pub static AX25_PROTOCOL_ID: u8 = 0xF0;
+pub const AX25_PROTOCOL_ID: u8 = 0xF0;
 /// A good place to split AX.25 address from information fields.
-pub static ADDR_INFO_DELIM: String = format!("{}{}", &AX25_CONTROL_FIELD, &AX25_PROTOCOL_ID);
+pub const ADDR_INFO_DELIM: [u8; 2] = [0x03, 0xF0];
 
 lazy_static! {
     pub static ref DATA_TYPE_MAP: HashMap<&'static str, &'static str> = {
-        let mut d = HashMap::new();
+        let mut d: HashMap<&'static str, &'static str> = HashMap::new();
         d.insert(">", "status");
         d.insert("!", "position_nots_nomsg");
         d.insert("=", "position_nots_msg");
@@ -45,4 +45,4 @@ lazy_static! {
 
 /// KISS Command Codes
 /// https://en.wikipedia.org/wiki/KISS_(TNC)#Command_Codes
-pub static KISS_DATA_FRAME: u8 = 0x00;
+pub const KISS_DATA_FRAME: u8 = 0x00;

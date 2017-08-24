b6b80278fb1a45e32d13e4d2b9e8b1c1f4e4c50f
italic <italic.rendezvous@gmail.com>
2017-08-24 00:12:26 -0700

    Public modules in top-level.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate lazy_static;
 
-mod constants;
+pub mod constants;
 pub use constants::{
     // LOG_FORMAT, LOG_LEVEL,
     APRSIS_SW_VERSION, APRSIS_HTTP_HEADERS, APRSIS_SERVERS,
@@ -11,22 +11,22 @@ pub use constants::{
     ADDR_INFO_DELIM, DATA_TYPE_MAP, KISS_DATA_FRAME
 };
 
-mod util;
+pub mod util;
 pub use util::{valid_callsign};
 
-mod geo_util;
+pub mod geo_util;
 pub use geo_util::{dec2dm_lat, dec2dm_lng, ambiguate};
 
-mod fcs;
+pub mod fcs;
 pub use fcs::{FCS};
 
-mod functions;
+pub mod functions;
 pub use functions::{
     parse_frame, parse_callsign,
     parse_callsign_ax25, parse_info_field
 };
 
-mod structs;
+pub mod structs;
 pub use structs::{
     Frame, Callsign, APRS, TCP, UDP,
     HTTP, InformationField, PositionFrame

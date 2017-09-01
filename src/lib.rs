418e6476698ba35cee55cfe562aea632df0a3034
italic <italic.rendezvous@gmail.com>
2017-09-01 00:49:13 -0700

    APRS: docs and credits. Still filling out information; will do when crate is more complete.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+//! This crate provides connectivity to the APRS amateur radio network over the internet.
+//!
+//! APRS (Automatic Packet Reporting System) is a packet-based, internet- and rf-connected
+//! communication network for amateur radio operators and enthusiasts that allows transport
+//! of location data, weather reports and more among operators. See the [APRS website][1]
+//! for a detailed description of the network.
+//!
+//! Credits:
+//! --------
+//! APRS (c) Bob Bruninga, WB4APR  
+//! [Original Python module][2] by Greg Albrecht, W2GMD  
+//! Rust port by Jeffrey Hoover, KI6SGV  
+//!
+//! This library is licensed under the Apache 2.0 license. See [LICENSE][2] for full terms.
+//!
+//! [1]: http://www.aprs.org/
+//! [2]: https://www.github.com/ampledata/aprs
+//! [3]: https://www.apache.org/licenses/LICENSE-2.0
+
 #[macro_use]
 extern crate lazy_static;
 
@@ -5,35 +24,10 @@ extern crate lazy_static;
 extern crate log;
 
 pub mod constants;
-pub use constants::{
-    // LOG_FORMAT, LOG_LEVEL,
-    APRSIS_SW_VERSION, APRSIS_HTTP_HEADERS, APRSIS_SERVERS,
-    APRSIS_FILTER_PORT, APRSIS_RX_PORT, APRSIS_URL,
-    AX25_FLAG, AX25_CONTROL_FIELD, AX25_PROTOCOL_ID,
-    RECV_BUFFER, DEFAULT_TOCALL,
-    ADDR_INFO_DELIM, DATA_TYPE_MAP, KISS_DATA_FRAME
-};
-
 pub mod util;
-pub use util::{valid_callsign};
-
 pub mod geo_util;
-pub use geo_util::{dec2dm_lat, dec2dm_lng, ambiguate};
-
 pub mod fcs;
-pub use fcs::{FCS};
-
 pub mod functions;
-pub use functions::{
-    parse_frame, parse_callsign,
-    parse_callsign_ax25, parse_info_field
-};
-
 pub mod structs;
-pub use structs::{
-    Frame, Callsign, APRS, TCP, UDP,
-    HTTP, InformationField, PositionFrame
-};
-
 pub mod decimaldegrees;
 pub mod kiss_structs;

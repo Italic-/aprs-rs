f9f23e474ebd8933e2b788e3349632613a03df2e
italic <italic.rendezvous@gmail.com>
2017-09-30 17:57:29 -0700

    Tests: barebones FCS test.
    Also bookmark style to include separate test files (tests/test_constants.rs, etc).

---
+++ tests/test_fcs.rs
@@ -0,0 +1,19 @@
+extern crate aprs;
+use aprs::fcs;
+
+extern crate data_encoding;
+use data_encoding::HEXLOWER as HEX;
+
+#[path="../tests/test_constants.rs"]
+mod test_constants;
+
+#[test]
+//#[ignore]
+fn test_fcs_checksum() {
+}
+
+
+#[test]
+#[ignore]
+fn test_fcs_validate() {
+}

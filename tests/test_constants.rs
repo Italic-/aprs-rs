ac8a5c1d454c75ed1ea78824ca271107cc2fded4
italic <italic.rendezvous@gmail.com>
2017-09-30 17:29:35 -0700

    Tests: constants for use across test suite

---
+++ tests/test_constants.rs
@@ -0,0 +1,10 @@
+pub static PANGRAM: &'static str = "the quick brown fox jumps over the lazy dog";
+pub static ALPHABET: &'static str = "abcdefghijklmnopqrstuvwxyz";
+pub static NUMBERS: &'static str = "0123456789";
+pub static POS_NUMBERS: &'static str = "123456789";
+pub static ALPHANUM: &'static str = "abcdefghijklmnopqrstuvwxyz0123456789";
+pub static TEST_HEX_FRAME: &'static str = {
+    "82a0a4b0646860ae648e9a88406cae92888a62406303f021333734352e37354e493\
+     1323232382e303557235732474d442d3620496e6e65722053756e7365742c205346\
+     2069476174652f4469676970656174657220687474703a2f2f7732676d642e6f7267"
+};
